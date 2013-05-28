02010c04509df8e50019f37789295fb9ae0b1896
Drew Parroccini <aparroccini@gmail.com>
2013-05-28 14:25:16 -0400

    Updated notes.scss, removed index.html

--- config.rb
+++ config.rb
@@ -1,6 +1,5 @@
 # Require any additional compass plugins here.
 
-# Set this to the root of your project when deployed:
 http_path = "/"
 css_dir = "stylesheets"
 sass_dir = "sass"
@@ -13,8 +12,7 @@ javascripts_dir = "javascripts"
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
 
-# To disable debugging comments that display the original location of your selectors. Uncomment:
-# line_comments = false
+line_comments = false
 
 
 # If you prefer the indented syntax, you might want to regenerate this
